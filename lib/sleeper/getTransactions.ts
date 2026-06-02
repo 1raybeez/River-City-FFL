0e09fd34eb2f4729a0bf1e3c48ad0171fba28886
Raymond Long <raylong1977@gmail.com>
2026-06-02 18:33:18 -0400

    Consolidate Sleeper helper exports

--- lib/sleeper/getTransactions.ts
+++ lib/sleeper/getTransactions.ts
@@ -1,16 +1,1 @@
-// ---------------------------------------------------------
-// File: /lib/sleeper/getTransactions.ts
-// Purpose: Fetch Sleeper league transactions for a given week
-// ---------------------------------------------------------
-
-export async function getTransactions(week: number, leagueId: string) {
-  const url = `https://api.sleeper.app/v1/league/${leagueId}/transactions/${week}`;
-
-  const res = await fetch(url);
-
-  if (!res.ok) {
-    throw new Error(`Failed to fetch transactions for week ${week}`);
-  }
-
-  return res.json();
-}
+export { getTransactions } from "../sleeper";
